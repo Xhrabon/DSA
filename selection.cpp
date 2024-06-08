71e45568811acffce0f70b8c83b873900d0eb50d
Shrabon Somadder <130784151+Xhrabon@users.noreply.github.com>
2024-06-09 00:03:20 +0600

    Update selection.cpp

--- selection.cpp
+++ selection.cpp
@@ -41,3 +41,56 @@ int main()
     display(arr);
 
 }
+
+/*
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n)
+{
+    int min_idx;
+
+    for(int i = 0; i<n-1; i++)
+    {
+        min_idx = i;
+        for(int j = i+1; j<n; j++)
+        {
+            if(arr[j]<arr[min_idx])
+            {
+                min_idx = j;
+
+            }
+        }
+        if(min_idx != i)
+            {
+                swap(arr[min_idx], arr[i]);
+            }
+    }
+}
+
+void display(int arr[], int s)
+{
+    for(int i = 0; i<s; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i = 0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    selection_sort(arr, n);
+    display(arr,n);
+
+
+}
+*/
